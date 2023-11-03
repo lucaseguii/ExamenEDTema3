9ee3f15dcca6d8ee8f725d307b8490b317ac5ca0
Luca Seguí <lsegui20216@iesjoanramis.org>
2023-11-03 17:57:18 +0100

    Comentarios javadoc añadidos

--- FuncionSuma.java
+++ FuncionSuma.java
@@ -1,15 +1,25 @@
 import java.util.Scanner;
-
+/** Porgrama suma, para calcular la suma de dos valores introducidos por teclado
+ * @version v1.0
+ * @author Miquel
+ * @see Para más información <a href="google.es">GOOGLE</a>
+ */
 
 
 public class FuncionSuma {
-
+/**
+ * Variable para guardar el primer sumando
+ */
 	
 	
 	private static int primerSumando;
-	
+	/**
+	 * Variable para guardar el segundo sumando
+	 */
 	private static int segundoSumando; 
-	
+	/**
+	 * Funcion principal del programa
+	 */
 	public static void main(String[] args) {
 		Scanner in = new Scanner(System.in);
 		
@@ -26,7 +36,9 @@ public class FuncionSuma {
 
 	}
 	
-	
+	/**
+	 * Funcion que realiza la suma de los dos operadores
+	 */
 	
 	private static int suma (int sumando1, int sumando2) {
 		return sumando1+sumando2;
